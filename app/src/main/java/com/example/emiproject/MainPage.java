52a3d2f5a0344ce6b997665da6254b32d906c472
Shiv Patel <da.shiv.patel@gmail.com>
2024-09-30 22:40:31 -0400

    Added dropdowns and calculations

--- app/src/main/java/com/example/emiproject/MainPage.java
+++ app/src/main/java/com/example/emiproject/MainPage.java
@@ -1,14 +1,26 @@
 package com.example.emiproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.text.DecimalFormat;
 
 public class MainPage extends AppCompatActivity {
+
+    TextView textView;
+    EditText principleAmount;
+    Spinner interestRateSpinner;
+    Spinner amortizationPeriodSpinner;
+    TextView resultBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,52 @@ public class MainPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textView = findViewById(R.id.pageHeader);
+        principleAmount = findViewById(R.id.principleAmountInput);
+        interestRateSpinner = findViewById(R.id.spinnerInterest);
+        amortizationPeriodSpinner = findViewById(R.id.spinnerAmortizationPeriod);
+        resultBox = findViewById(R.id.resultBox);
+
+        String[] interestRates = {
+                "3 Year Fixed Rate 4.84%",
+                "5 Year Fixed Rate 4.74%",
+                "1 Year Closed 7.34%",
+                "5 Year Closed Variable Rate 6.14%",
+                "5 Year Open Variable Rate 7.6%"
+        };
+        ArrayAdapter<String> interestRateAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, interestRates);
+        interestRateSpinner.setAdapter(interestRateAdapter);
+
+        String[] years = {
+                "10 years", "15 years", "20 years", "25 years", "30 years"
+        };
+        ArrayAdapter<String> amortizationPeriodAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, years);
+        amortizationPeriodSpinner.setAdapter(amortizationPeriodAdapter);
+
+    }
+    public void calculateEMI(View view) {
+        String principleInput = principleAmount.getText().toString();
+
+        if (principleInput.isEmpty()) {
+            resultBox.setText("@string/principleError");
+        }
+
+        String interestRateInput = interestRateSpinner.getSelectedItem().toString();
+        String amortizationPeriodInput = amortizationPeriodSpinner.getSelectedItem().toString();
+
+        double interestRate = Double.parseDouble(interestRateInput.replaceAll("[^0-9]",""));
+        int years = Integer.parseInt(amortizationPeriodInput.replaceAll("[^0-9]",""));
+        double principle = Double.parseDouble(principleInput);
+
+        double monthlyRate = interestRate / 12 / 100;
+        double amortizationMonths = years * 12;
+
+        double emi = ((principle * monthlyRate * Math.pow((1 + monthlyRate), amortizationMonths))/(Math.pow(1 + monthlyRate, amortizationMonths)) - 1);
+
+        DecimalFormat decFormat = new DecimalFormat("#.##");
+        String formattedFinalAnswer = decFormat.format(emi);
+
+        resultBox.setText("Your monthly EMI is: $" + formattedFinalAnswer);
     }
 }
